3cb668cc4efa9d7729775338061e41d580fe7258
AtsushiA <ando@next-season.net>
2016-10-19 17:41:08 +0900

    add ClearData

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ## ClearCaches
-
 rm -Rf /opt/codedeploy-agent/deployment-root/*
 rm -Rf /tmp/codedeploy
 
 ## Stop Apache
 sudo /sbin/service httpd stop
+
+## ClearData
+sudo rm -Rf /var/www/html/*
